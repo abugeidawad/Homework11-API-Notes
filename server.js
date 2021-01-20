47d8f70be7bcfd70c50a8f5b36c23ccf20baf3d9
abugeidawad <69802124+abugeidawad@users.noreply.github.com>
2021-01-20 18:16:21 +0800

    express

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/notes', (req, res) => {
+  res.json(
+      [
+          {
+              "title": "my first note",
+              "textarea":"my first text area"
+          },
+          {
+            "title": "my second note",
+            "textarea":"my second text area"
+        }
+      ]
+  )
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
